e8b1d8d088990097451c0d271e7de228607a4b37
fulopmartin <fulopmartin.fm@gmail.com>
2026-05-06 13:27:32 +0200

    Menü, reszponzivitás
    Függőleges menü átalakítása vízszintes menüvé, reszponzív tervezés.

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -4,7 +4,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?= $ablakcim['cim'] . ( (isset($ablakcim['mottó'])) ? ('|' . $ablakcim['mottó']) : '' ) ?></title>
 	<link rel="stylesheet" href="./styles/stilus.css" type="text/css">
 	<?php if(file_exists('./styles/'.$keres['fajl'].'.css')) { ?><link rel="stylesheet" href="./styles/<?= $keres['fajl']?>.css" type="text/css"><?php } ?>
